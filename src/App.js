d090e23d7819efe516a233b11c6e972844c8ac25
Dipendra Nath <dipen.2052@gmail.com>
2024-02-10 19:55:40 -0500

    router setup for different pages

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
 function App() {
   return (
     <div className="App">
-
+      <Router>
+        <Routes>
+          <Route path="/" element={<h1>Home</h1>} />
+          <Route path="/product/:productID" element={<h1>Product_detail</h1>} />
+            <Route path="/cart" element={<h1>Cart</h1>} />
+          <Route path="*" element={<h1>Not Found</h1>} />
+        </Routes>
+      </Router>
 
     </div>
   );
